6b062e6baee164bbc418b0cae85394df101a60a6
Jonathan Tay <jonathantaymy@gmail.com>
2022-06-26 11:17:01 +0800

    Add deployment server code

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const passport = require("passport");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const https = require("https");
+const fs = require("fs");
 
 // Setup
 require("dotenv").config();
@@ -15,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
     cors({
-        origin: "http://localhost:3000", // <-- location of the react app were connecting to
+        origin: process.env.FRONTEND_URL,
         credentials: true,
     })
 );
@@ -33,6 +35,23 @@ app.use("/reset-password", require("./routes/resetPassword"));
 app.use("/api/upload", require("./routes/upload"));
 
 // Server
-app.listen(4000, () => {
-    console.log("Server started on port 4000.");
-});
+if (process.env.NODE_ENV === "production") {
+    const privateKey = fs.readFileSync(process.env.PRIVATE_KEY_PATH, "utf8");
+    const certificate = fs.readFileSync(process.env.CERTIFICATE_PATH, "utf8");
+    const ca = fs.readFileSync(process.env.CHAIN_PATH, "utf8");
+
+    const credentials = {
+        key: privateKey,
+        cert: certificate,
+        ca: ca,
+    };
+
+    const httpsServer = https.createServer(credentials, app);
+    httpsServer.listen("8443", () => {
+        console.log("Server started at https://dollarplanner.live:8443.");
+    });
+} else {
+    app.listen(4000, () => {
+        console.log("Server started on port 4000.");
+    });
+}
