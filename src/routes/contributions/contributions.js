c72eb889dab0d9b453dd2be85576b639acb4f160
huangzz125 <huangzz125@gmail.com>
2022-07-21 01:29:58 +0800

    Implemented unlike function

--- src/routes/contributions/contributions.js
+++ src/routes/contributions/contributions.js
@@ -49,7 +49,8 @@ router.put(
     }
 );
 
-//Update contribution with user who liked it and like count.
+// Update contribution with user who liked it
+// Update user with the id of contribution liked
 router.put(
     "/like/:id",
     passport.authenticate("jwt", { session: false }, undefined),
@@ -64,6 +65,35 @@ router.put(
             });
             await User.findByIdAndUpdate(UserId, {
                 $push: {
+                    likedContributions: contributionId,
+                },
+            });
+            res.status(200).send("Contribution reported successfully.");
+        } catch {
+            res.status(500).send({
+                title: "Something went wrong.",
+                description: "Please try again.",
+            });
+        }
+    }
+);
+
+// Update contribution with user who liked it
+// Update user with the id of contribution liked
+router.put(
+    "/unlike/:id",
+    passport.authenticate("jwt", { session: false }, undefined),
+    async (req, res) => {
+        try {
+            const UserId = await readIDFromRequestWithJWT(req);
+            const contributionId = req.params.id;
+            await Contribution.findByIdAndUpdate(contributionId, {
+                $pull: {
+                    likedBy: UserId,
+                },
+            });
+            await User.findByIdAndUpdate(UserId, {
+                $pull: {
                     likedContributions: contributionId,
                 },
             });
