dffaab6603f4e3524bae303ee06816827d26c129
Jonathan Tay <jonathantaymy@gmail.com>
2022-07-22 09:48:26 +0800

    Create portfolio schemas

---
+++ src/models/portfolio/Liability.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const liabilitySchema = new mongoose.Schema(
+    {
+        user_id: mongoose.Schema.Types.ObjectId,
+        createdAt: Date,
+        name: String,
+        description: String,
+        amount: mongoose.Schema.Types.Decimal128,
+        interest: mongoose.Schema.Types.Decimal128,
+    },
+    { collection: "liabilities" }
+);
+
+module.exports = mongoose.model("Liability", liabilitySchema);
---
+++ src/models/portfolio/Payment.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+const paymentSchema = new mongoose.Schema({
+    user_id: mongoose.Schema.Types.ObjectId,
+    liability_id: mongoose.Schema.Types.ObjectId,
+    createdAt: Date,
+    amount: mongoose.Schema.Types.Decimal128,
+});
+
+module.exports = mongoose.model("Payment", paymentSchema);
