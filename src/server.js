cd0336627cf1ecc2579ce9ced54b733f8d7f0fdb
jontmy <jonathantaymy@gmail.com>
2023-01-18 06:06:48 +0000

    Fix CORS error from missing domain

--- src/server.js
+++ src/server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
     cors({
-        origin: process.env.FRONTEND_URL,
+        origin: [process.env.FRONTEND_URL, process.env.FRONTEND_WWW_URL],
         credentials: true,
     })
 );
